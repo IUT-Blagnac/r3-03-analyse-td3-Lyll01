f6c78878247b94927ac33c3956889f84c5792b44
Lyll01 <dert3k@gmail.com>
2023-09-21 12:05:01 +0200

    hello world

---
+++ helloWorld/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,20 @@
+package $org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+    public static void hello() {
+        System.out.print("Hello World");
+    }
+
+    public static String hello(String parametre) {
+        return parametre;
+    }
+}
